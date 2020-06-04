173e2f79fefe3aec8b516545befbb874a635bf4b
Flians <rongliangfu@outlook.com>
2020-06-04 18:59:59 +0800

    repair Graph_Map_CSR

--- src/main/java/com/antfin/graph/refObj/Graph_Map_CSR.java
+++ src/main/java/com/antfin/graph/refObj/Graph_Map_CSR.java
@@ -6,6 +6,7 @@ import com.antfin.graph.Graph;
 import com.antfin.util.GraphHelper;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @param <K>
@@ -22,6 +23,7 @@ import java.util.*;
  * @date 2020/5/18 14:29
  */
 public class Graph_Map_CSR<K, VV, EV> extends Graph<K, VV, EV> {
+    private static Vertex nullVertex = new Vertex();
     private List<Vertex<K, VV>> vertices;
     private List<List<Edge<K, EV>>> edges;
     // dict_V[vertex.id] -> index, record all vertices
@@ -50,36 +52,69 @@ public class Graph_Map_CSR<K, VV, EV> extends Graph<K, VV, EV> {
     @Override
     public void addVertex(Vertex<K, VV> vertex) {
         if (!this.dict_V.containsKey(vertex.getId())) {
-            this.vertices.add(vertex);
+            if (vertex.isEmpty()) {
+                this.vertices.add(nullVertex);
+            } else {
+                this.vertices.add(vertex);
+            }
             this.dict_V.put(vertex.getId(), this.dict_V.size());
+        } else {
+            if (vertex.isEmpty()) {
+                this.vertices.set(this.dict_V.get(vertex.getId()), nullVertex);
+            } else {
+                this.vertices.set(this.dict_V.get(vertex.getId()), vertex);
+            }
         }
     }
 
     @Override
     public void addEdge(Edge<K, EV> edge) {
-        // the source vertex of edge has no edges.
-        if (!this.dict_V.containsKey(edge.getSrcId())) {
-            this.vertices.add(new Vertex<>(edge.getSrcId()));
-            this.dict_V.put(edge.getSrcId(), this.edges.size());
-            this.dict_V.put(this.vertices.get(this.edges.size()).getId(), this.dict_V.size() - 1);
-            GraphHelper.swap(this.edges.size(), this.vertices.size() - 1, this.vertices);
-            List<Edge<K, EV>> item = new ArrayList<>();
-            item.add(edge);
-            this.edges.add(item);
-        } else {
+        K srcId = edge.getSrcId();
+        boolean exist = dict_V.containsKey(srcId);
+        if (exist && this.dict_V.get(srcId) < this.edges.size()) {
             // the source vertex of edge has other edges.
-            this.edges.get(this.dict_V.get(edge.getSrcId())).add(edge);
+            this.edges.get(this.dict_V.get(srcId)).add(edge);
+        } else {
+            int _id = 0;
+            if (!exist) {
+                this.vertices.add(nullVertex);
+                if (this.dict_V.size() > 0) {
+                    _id = this.dict_V.size() - 1;
+                }
+            } else {
+                _id = this.dict_V.get(srcId);
+            }
+            this.dict_V.put(srcId, this.edges.size());
+            this.dict_V.put(this.vertices.get(this.edges.size()).getId(), _id);
+            GraphHelper.swap(this.edges.size(), _id, this.vertices);
+
+            List<Edge<K, EV>> edges = new ArrayList<>();
+            edges.add(edge);
+            this.edges.add(edges);
         }
     }
 
     @Override
-    public Vertex<K, VV> getVertex(K id) {
-        return this.vertices.get(this.dict_V.get(id));
+    public Vertex<K, VV> getVertex(K sid) {
+        Vertex<K, VV> result = this.dict_V.containsKey(sid) ? this.vertices.get(this.dict_V.get(sid)) : null;
+        if (result == nullVertex) {
+            return new Vertex<>(sid);
+        } else {
+            return result;
+        }
     }
 
     @Override
     public List<Edge<K, EV>> getEdge(K sid) {
-        return this.edges.get(this.dict_V.get(sid));
+        if (this.dict_V.containsKey(sid) && this.dict_V.get(sid) < this.edges.size()) {
+            return this.edges.get(this.dict_V.get(sid)).stream()
+                .map(x -> {
+                    x.setSrcId(sid);
+                    return x;
+                })
+                .collect(Collectors.toList());
+        }
+        return new ArrayList<>();
     }
 
     @Override
